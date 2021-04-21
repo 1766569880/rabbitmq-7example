9ac5030e2f19ca9650f876154b4722a621414a70
1766569880 <1766569880@qq.com>
2021-04-21 23:05:13 +0800

    4.21 两个demo

--- src/main/java/org/example/work/fair/recv/Recv.java
+++ src/main/java/org/example/work/fair/recv/Recv.java
@@ -1,15 +1,16 @@
-package org.example.simple.recv;
+package org.example.work.fair.recv;
 
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
+
 /*
-* 简单队列-消费者
+* 工作队列-工平 消费者1
 * */
 public class Recv {
 
-    private final static String QUEUE_NAME = "hello";
+    private final static String QUEUE_NAME = "work_fair";
 
     public static void main(String[] argv) throws Exception {
         //创建连接工厂 port默认不用配置
@@ -24,14 +25,27 @@ public class Recv {
         Channel channel = connection.createChannel();
         //绑定队列
         channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+        //限制消费者每次只能接收一（prefetchCount）条消息，处理完才能接收下一条
+        int prefetchCount = 1;
+        channel.basicQos(prefetchCount);
+        
         System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             //拿到消息
             String message = new String(delivery.getBody(), "UTF-8");
             System.out.println(" [x] Received '" + message + "'");
+            //手动确认 参数：multiple是否确认多条-》false逐条确认
+            channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
         };
         //监听队列 消费消息
-        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
+        //队列名称 自动回执
+        channel.basicConsume(QUEUE_NAME, false, deliverCallback, consumerTag -> { });
     }
 }
--- src/main/java/org/example/work/rr/recv/Recv2.java
+++ src/main/java/org/example/work/rr/recv/Recv2.java
@@ -1,15 +1,16 @@
-package org.example.simple.recv;
+package org.example.work.rr.recv;
 
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
-/*
-* 简单队列-消费者
-* */
-public class Recv {
 
-    private final static String QUEUE_NAME = "hello";
+/*
+* 工作队列-轮询 消费者1
+* */
+public class Recv2 {
+
+    private final static String QUEUE_NAME = "work_rr";
 
     public static void main(String[] argv) throws Exception {
         //创建连接工厂 port默认不用配置
@@ -27,11 +28,19 @@ public class Recv {
         System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             //拿到消息
             String message = new String(delivery.getBody(), "UTF-8");
             System.out.println(" [x] Received '" + message + "'");
+            //手动确认 参数：multiple是否确认多条-》false逐条确认
+            channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
         };
         //监听队列 消费消息
-        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> { });
+        //队列名称 自动回执
+        channel.basicConsume(QUEUE_NAME, false, deliverCallback, consumerTag -> { });
     }
 }
